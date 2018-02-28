a07d4d7375b877abdd992af9d0dbe855bcddaf75
platinum <platinumindustries@users.noreply.github.com>
2018-02-28 12:14:36 +0300

    Update index.js

--- html/index.js
+++ html/index.js
@@ -1,5 +1,13 @@
+const Koa = require('koa');
+const app = new Koa();
+
 class index {
-  
+  constructor() {
+    app.use(async ctx => {
+      ctx.body = 'Hello World';
+    });
+    app.listen(81);
+  }
 }
 
-new index()
+new index();
